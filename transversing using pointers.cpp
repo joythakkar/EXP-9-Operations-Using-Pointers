f9bf3afd6e0761e02675b166ee7a86188c9feafb
Joy Thakkar <joythakkar20406@gmail.com>
2025-08-18 22:35:11 +0530

    Update transversing using pointers.cpp

--- transversing using pointers.cpp
+++ transversing using pointers.cpp
@@ -1,16 +1,25 @@
-#include <iostream>
+// Joy Thakkar 
+// 24070123051
+
+#include <iostream> 
 using namespace std;
 
-int main() {
-    int arr[] = {1, 11, 111, 1111, 11111};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    int* ptr = &arr[n - 1];
-
-    for (int i = n - 1; i >= 0; i--) {
-        cout << *ptr << " ";
-        ptr--; 
+int main(){
+    // Reversing an array using pointers
+    int array[15] = {5,6,7,22,3,1,4,6,0,3,6,6,2,2,5};
+    int len = (sizeof(array)/sizeof(array[0]));
+    int *strt_ptr,*end_ptr;
+    strt_ptr = &array[0];
+    end_ptr = &array[len - 1];
+    
+    while(end_ptr >= strt_ptr){
+        cout<< *end_ptr <<"\t";
+        end_ptr--;
     }
-
     return 0;
 }
+
+/*
+Sample Output: 
+5	2	2	6	6	3	0	6	4	1	3	22	7	6	5
+*/
